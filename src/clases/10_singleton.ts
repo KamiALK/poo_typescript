517f8500037b04f1263be48e69206bf2afc84659
kamilin laptop <andresyalca@gmail.com>
2025-06-01 21:34:02 -0500

    singleton pero no se para que se usa

---
+++ src/clases/10_singleton.ts
@@ -0,0 +1,14 @@
+export class MyService {
+  constructor(public name: String) { }
+  getName() {
+    return this.name;
+  }
+
+}
+
+const myServicea = new MyService("kamilo");
+const myServiceb = new MyService("alca");
+console.log(myServiceb === myServicea)
+
+console.log(myServicea.getName())
+console.log(myServiceb.getName())
