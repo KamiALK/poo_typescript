5eac3dcb7664b6b0e4e6a7efc634e1711457d3cd
kamilin laptop <andresyalca@gmail.com>
2025-08-09 12:15:41 -0500

    quite espacio dentro del from './protegida'

--- src/clases/12-classes_abstractas.ts
+++ src/clases/12-classes_abstractas.ts
@@ -1,1 +1,9 @@
-import { clase_protegida } from './protegida ';
+
+import { clase_protegida } from './protegida';
+
+// creando instancia de objeto
+
+const clase_protegida_uno = new clase_protegida('kamilo', 'almanza');
+clase_protegida_uno.saludar
+
+
